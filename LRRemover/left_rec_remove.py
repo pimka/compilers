d6d2dff6156fdaf0a9f769356845cd5970f0327b
pimka <zelenova.yulia@mail.ru>
2020-03-24 20:31:45 +0300

    Added left recursion remover

---
+++ LRRemover/left_rec_remove.py
@@ -0,0 +1,92 @@
+import json
+
+class Grammar:
+    def __init__(self, path_to_json):
+        dict_temp = self.__jsonParser(path_to_json)
+        self.terminals = dict_temp['terminals']
+        self.nonterminals = dict_temp['nonterminals']
+        self.productions = dict_temp['productions']
+        self.startSymbolName = dict_temp['startSymbolName']
+
+    def __jsonParser(self, path_to_json):
+        file = open(path_to_json)
+        json_file = json.load(file)
+        file.close()
+        return json_file
+
+    def remove_recursion(self):
+        for i in range(0, len(self.nonterminals)):
+            for j in range(0, i):
+                prods = self.__findProduction(self.nonterminals[i], self.nonterminals[j])
+                left_prods = self.__findProductionsByLeft(self.nonterminals[j])
+                for pr in prods:
+                    self.productions.remove(pr)
+                    for lpr in left_prods:
+                        self.productions.append(self.__createProduction(pr, self.nonterminals[j], lpr))
+            
+            prods = self.__findProduction(self.nonterminals[i], self.nonterminals[i])
+            if prods:
+                left_prods = self.__findProductionsByLeft(self.nonterminals[i])
+                for pr in left_prods:
+                    if pr not in prods:
+                        pr['right'].append(
+                            {
+                                'isTerminal' : 'False',
+                                'name' : f'{self.nonterminals[i]}\''
+                            })
+                    else:
+                        self.productions.remove(pr)
+                        prod = { 'left' : f'{self.nonterminals[i]}\'', 'right' : pr['right'] }
+                        for r in pr['right']:
+                            if r['name'] == self.nonterminals[i]:
+                                prod['right'].remove(r)
+                                prod['right'].append(
+                                    {
+                                        'isTerminal' : 'False',
+                                        'name' : f'{self.nonterminals[i]}\''
+                                    })
+
+                        self.productions.append(prod)
+
+                self.productions.append({
+                    'left' : f'{self.nonterminals[i]}\'',
+                    'right' : [
+                        {
+                            'isTerminal' : 'True',
+                            'name' : 'e'
+                        }
+                    ]
+                })
+
+    def __findProduction(self, left, right):
+        result = []
+        right = {'isTerminal':'False', 'name':right}
+        for prod in self.productions:
+            if prod['left'] == left and right in prod['right']:
+                result.append(prod)
+
+        return result
+
+    def __findProductionsByLeft(self, left):
+        result = []
+        for prod in self.productions:
+            if prod['left'] == left:
+                result.append(prod)
+
+        return result
+
+    def __createProduction(self, prod, replaceNonterminal, replaceProd):
+        prod_copy = prod.copy()
+        copy_right = []
+        for i in prod['right']:
+            if i['name'] == replaceNonterminal:
+                copy_right.extend(replaceProd['right'])
+            else:
+                copy_right.append(i)
+        prod_copy['right'] = copy_right
+
+        return prod_copy
+
+gr = Grammar('LRRemover/test_grammar.json')
+gr.remove_recursion()
+print()
